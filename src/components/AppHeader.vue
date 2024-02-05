9f0721d07061e21b58bdd06be4fe938ad6814da5
LucaB98 <lucabellan98@gmail.com>
2024-02-05 16:32:13 +0100

    add AppHeader

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+    name: 'AppHeader',
+};
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <ul class="my-3">
+                <li>
+                    <div class="blue m-2"></div>
+                </li>
+                <li>
+                    <div class="cube-small red m-2"></div>
+                </li>
+                <li>
+                    <div class="cube-small yellow m-2"></div>
+                </li>
+                <li>
+                    <div class="cube-small green m-2"></div>
+                </li>
+            </ul>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+ul {
+    list-style-type: none;
+    display: flex;
+}
+
+.blue {
+    height: 100px;
+    width: 100px;
+    background-image: linear-gradient(to bottom right, white, blue);
+    border: 5px solid white;
+    border-radius: 50%;
+}
+
+.cube-small {
+    height: 50px;
+    width: 50px;
+    border: 3px solid white;
+    border-radius: 50%;
+}
+
+.red {
+    background-image: linear-gradient(to bottom right, white, red);
+}
+
+.yellow {
+    background-image: linear-gradient(to bottom right, white, yellow);
+}
+
+.green {
+    background-image: linear-gradient(to bottom right, white, green);
+}
+</style>

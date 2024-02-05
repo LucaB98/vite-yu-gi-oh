4004145ff43651b57dd336e96cf58fe85e56d8b2
LucaB98 <lucabellan98@gmail.com>
2024-02-05 16:32:03 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'PokeDex',
+  components: { AppHeader }
+};
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style lang="scss">
+@use './assets/scss/style.scss';
+</style>
